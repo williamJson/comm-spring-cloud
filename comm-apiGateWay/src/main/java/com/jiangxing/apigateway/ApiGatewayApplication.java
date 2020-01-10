49f909320c6e200a20a684b762c6761ee1e5319a
wyq <342622023@qq.com>
2020-01-10 08:44:51 +0800

    zuul

--- comm-apiGateWay/src/main/java/com/jiangxing/apigateway/ApiGatewayApplication.java
+++ comm-apiGateWay/src/main/java/com/jiangxing/apigateway/ApiGatewayApplication.java
@@ -3,7 +3,9 @@ package com.jiangxing.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * 服务网关
@@ -11,7 +13,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @author wyq
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
