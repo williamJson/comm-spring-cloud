8ce0afb990b1945ff01a465e666fbf2e690acef9
william <342622023@qq.com>
2020-01-15 15:22:51 +0800

    mysql

--- user-center/src/main/java/com/jiangxing/user/UserApplication.java
+++ user-center/src/main/java/com/jiangxing/user/UserApplication.java
@@ -48,4 +48,6 @@ public class UserApplication {
         LettuceConnectionFactory factory = new LettuceConnectionFactory(config);
         return factory;
     }
+
+
 }
--- user-center/src/test/java/com/jiangxing/user/RedisTest.java
+++ user-center/src/test/java/com/jiangxing/user/RedisTest.java
@@ -4,11 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import javax.annotation.Resource;
 
 /**
  * Created by wyq on 2020/1/13.
@@ -25,16 +25,32 @@ public class RedisTest {
     public RedisTemplate<String, String> template;
 
 
-    @Resource(name = "redisTemplate")
-    public ValueOperations<String, String> operations;
+//    @Resource(name = "redisTemplate")
+//    public ValueOperations<String, String> operations;
+
 
     @Test
     public void addValue() {
         //这他娘的，set没有返回值，也不知道成功与否，只能再get一下哈？？？
-        //template.opsForValue().set("nihao", "nihao redis");
-        System.out.println(template.opsForValue().get("name"));
+//        template.opsForValue().set("nihao", "nihao redis");
+        // System.out.println(template.opsForValue().get("name"));
+        //System.out.println(template.opsForValue().getAndSet("testsbsbs", "test"));
 //        operations.set("ops", "ops");
 //        System.out.println(operations.get("ops"));
+//        System.out.println(template.execute(new RedisCallback<Boolean>() {
+//            @Override
+//            public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
+//                return connection.set("1234aaa".getBytes(), "5667aaa".getBytes());
+//            }
+//        }));
 
     }
+
+
+
+    @Test
+    public void conditionOnClassArray() {
+        System.out.println("start...");
+    }
+
 }
